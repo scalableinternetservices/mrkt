e1fc826521ef0a5c925944b3667e2b8e8f7e6085
Rakshit Garg <raks.garg@gmail.com>
2016-11-12 12:14:49 -0800

    finished 13.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,4 +38,11 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+   # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
 end
