3e7df285a86ef514f5c8600943640387afa10b31
Rakshit Garg <raks.garg@gmail.com>
2016-11-11 23:08:16 -0800

    added pictures posts and profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -47,7 +48,7 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  private
+   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
@@ -56,21 +57,13 @@ class UsersController < ApplicationController
 
     # Before filters
 
-  # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
-  # Confirms the correct user.
+    # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
 
+    # Confirms an admin user.
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
