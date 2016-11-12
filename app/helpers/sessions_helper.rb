0b2af29a6a48b8b3bff7d633b6de9658f321a757
Rakshit Garg <raks.garg@gmail.com>
2016-11-11 17:55:31 -0800

    implemented user update, edit, index, and destroy

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,13 +4,18 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-  
+
   # Remembers a user in a persistent session.
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
-  end  
+  end
+
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
 
   # Returns the user corresponding to the remember token cookie.
   def current_user
