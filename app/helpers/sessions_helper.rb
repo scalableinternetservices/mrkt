b40b884ab3076f5c17412844e94141c6c12e8ae0
Rohan Kapoor <rohanneilkapoor@gmail.com>
2016-11-14 23:14:30 -0800

    fixed login issue

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -47,5 +47,18 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
-  end  
+  end
+
+  def redirect_back_or(default)
+      redirect_to(session[:forwarding_url] || default)
+      session.delete(:forwarding_url)
+    end
+
+    # Stores the URL trying to be accessed.
+    def store_location
+      session[:forwarding_url] = request.original_url if request.get?
+    end
+
+
+
 end
